31e1a129755be9876d2c324a88b73244f0620c9f
GasparBritos <gasparbritos2011@gmail.com>
2023-11-22 22:19:17 -0300

    ADD sedMessage

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require('./routes/index.js');
 
 const server = express();
-
+server.use(express.json())
 server.use('/', routes);
 
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,10 @@
 const {Router} = require('express');
 const getAllCountries= require('../controllers/getAllCountries')
-
+const postMessage	=require('../controllers/postMessage')
 const router = Router();
 
 router.get("/getcountries", getAllCountries)
+router.post('/send-message', postMessage)
 
 
 module.exports = router;
