5179cb5a1451be4c9b8327cb75307b293015f546
GasparBritos <gasparbritos2011@gmail.com>
2023-11-23 14:29:35 -0300

    create new route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,14 @@
 const {Router} = require('express');
 const getAllCountries= require('../controllers/getAllCountries')
-const postMessage	=require('../controllers/postMessage')
+const postMessage	=require('../controllers/postMessage');
+const getAllUsers = require('../controllers/getAllUsers');
+
+
 const router = Router();
 
 router.get("/getcountries", getAllCountries)
 router.post('/send-message', postMessage)
+router.get('/users',getAllUsers)
 
 
 module.exports = router;
