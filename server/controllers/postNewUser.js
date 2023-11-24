176f7fdc13dac3740c1ed1654b5ea2fe07c2d1f4
GasparBritos <gasparbritos2011@gmail.com>
2023-11-24 00:12:42 -0300

    create postNewUser

---
+++ server/controllers/postNewUser.js
@@ -0,0 +1,38 @@
+const { db } = require('../firebase-confing');
+
+const postNewUser = async (req, res) => {
+    try {
+        
+        const { uid, name, lastname, age, isVip,sex,country, user } = req.body;
+
+        const result = await createUser(uid, name, lastname, age, isVip,sex,country, user);
+
+        res.status(200).json(result)
+    } catch (error) {
+        res.status(404).send('faltan requisitos')
+    }
+}
+
+const createUser= async (uid, name, lastname, age, isVip,sex,country, user)=>{
+
+    try {
+        const newUser= await db.collection('users').add({
+            uid,
+            name, 
+            lastname, 
+            age, 
+            isVip,
+            sex,
+            country,
+            user,
+            timestamp: new Date()
+        })
+        return  newUser
+        
+    } catch (error) {
+        console.error('Error sending user:', error);
+        return { success: false, error: 'Error sending user' };
+    }
+}
+
+module.exports=postNewUser;
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ const {Router} = require('express');
 const getAllCountries= require('../controllers/getAllCountries')
 const postMessage	=require('../controllers/postMessage');
 const getAllUsers = require('../controllers/getAllUsers');
+const postNewUser = require('../controllers/postNewUser');
 
 
 const router = Router();
@@ -9,6 +10,6 @@ const router = Router();
 router.get("/getcountries", getAllCountries)
 router.post('/send-message', postMessage)
 router.get('/users',getAllUsers)
-
+router.post('/newUser',postNewUser)
 
 module.exports = router;
