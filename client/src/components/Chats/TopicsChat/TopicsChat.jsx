c3aa16bc02f9bec9d34e2e38f2b14eeb45d43332
Maxxiandrade <maxiandrade62@gmail.com>
2023-11-26 00:34:59 -0300

    updated components

--- client/src/components/Chats/TopicsChat/TopicsChat.jsx
+++ client/src/components/Chats/TopicsChat/TopicsChat.jsx
@@ -1,31 +1,33 @@
-import { useState, useRef, useEffect } from "react";
+import {useRef } from "react";
 
 const TopicsChat = ({ setingValueRoom }) => {
   const roomInputRef = useRef(null);
-  const [optionRooms, setOptionRooms] = useState(['futbal', 'lenguaje', 'dogs', 'cats']);
 
-  const setValue = () => {
-    const selectedValue = document.getElementById('roomSelect').value;
-    const selectedRoom = selectedValue === 'null' ? null : selectedValue !== '' ? selectedValue : roomInputRef.current.value;
+  const handleTopic = (e)=>{
+    const topic = e.target.value
+    const selectedRoom = topic === 'null' ? null : topic !== '' ? topic : roomInputRef.current.value;
     setingValueRoom(selectedRoom);
-  };
+  }
 
   return (
     <div>
       <div className="room">
         <label htmlFor="roomSelect">Choose a topic</label>
         <section>
-          <select id="roomSelect">
-            {optionRooms.map((room, index) => (
-              <option key={index} value={room}>
-                {room}
-              </option>
-            ))}
+          <select id="roomSelect" onChange={handleTopic}>
+            <option value="global" >Global</option>
+            <option value="sports" >Sports</option>
+            <option value="animals" >Animals</option>
+            <option value="food" >Food</option>
+            <option value="tech" >Tech</option>
           </select>
         </section>
+        
+        
+        {/* funcion premium
         <button onClick={setValue}>
           Search topic chat
-        </button>
+        </button> */}
       </div>
     </div>
   );
