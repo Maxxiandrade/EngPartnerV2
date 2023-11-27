2272cae0ac2ce66e399e8181b5ef9c275d9b3ba5
GasparBritos <gasparbritos2011@gmail.com>
2023-11-26 21:22:29 -0300

    fixed the import user

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -16,7 +16,10 @@ import crown from "../../assets/crown.svg";
 import TopicsChat from "../Chats/TopicsChat/TopicsChat";
 import TopicChat from '../Chats/TopicChat/TopicChat'
 
+import { Navigate } from 'react-router-dom';
+
 const Home = ({ setIsAuth }) => {
+  const user = useSelector((state) => state.users.name);
   const dispatch = useDispatch();
   const uid = auth.currentUser?.uid;
 
@@ -38,43 +41,48 @@ const Home = ({ setIsAuth }) => {
   };
 
   useEffect(() => {
-    const fetchData = async () => {
-      await dispatch(getMyUser(uid));
-      // Ahora, el estado debería estar actualizado
-    };
-
-    fetchData();
-  }, [dispatch, uid]);
-
-  // Acceder a la propiedad name del estado
-  const user = useSelector((state) => state.users.name);
+    console.log(uid)
+    if ( uid === undefined) {
+      signOut(auth);
+      setIsAuth(false);
+    }
+    dispatch(getMyUser(uid));
+    console.log('holas')
+  }, [uid, dispatch]);
 
   return (
     <>
-      <nav className={style.nav}>
-      <img src={logo} className={style.logo}/>
+      {uid ? (
         <div>
-          <Link to={`/profile/${uid}`}>
-            <button className={style.profileBtn}>Profile</button>
-          </Link>
-          <Link to='/premium'>
-            <button className={style.premium}>
-            <img src={crown} alt="" className={style.crown}/>Get VIP
-            </button>
-          </Link>
-          <button onClick={handleLogOut} className={style.signOut}>Log out</button>
-          <Link to='/connect'>
-            <button className={style.connectBtn}>Connect</button>
-          </Link>
+          <nav className={style.nav}>
+            <img src={logo} className={style.logo}/>
+            <div>
+              <Link to={`/profile/${uid}`}>
+                <button className={style.profileBtn}>Profile</button>
+              </Link>
+              <Link to='/premium'>
+                <button className={style.premium}>
+                  <img src={crown} alt="" className={style.crown}/>Get VIP
+                </button>
+              </Link>
+              <button onClick={handleLogOut} className={style.signOut}>Log out</button>
+              <Link to='/connect'>
+                <button className={style.connectBtn}>Connect</button>
+              </Link>
+            </div>
+            <TopicsChat setingValueRoom={setingValueRoom} />
+          </nav>
+          <h2 className="">Welcome, {user} !</h2>
+          <div className={style.globalChat}>
+            <TopicChat room={room} setRoom={setRoom} />
+          </div>
         </div>
-        <TopicsChat setingValueRoom={setingValueRoom} />
-      </nav>
-      <h2 className="">Welcome, {user} !</h2>
-      <div className={style.globalChat}>
-        <TopicChat room={room} setRoom={setRoom} />
-      </div>
+      ) : (
+        <Navigate to="/" replace={true} />
+      )}
     </>
   );
 };
+
 
 export default Home;
