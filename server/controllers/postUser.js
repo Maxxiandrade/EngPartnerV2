967610f09fe74f7c01494da45969317e0230b70b
GasparBritos <gasparbritos2011@gmail.com>
2023-11-27 18:37:51 -0300

    fixed back mail

--- server/controllers/postUser.js
+++ server/controllers/postUser.js
@@ -2,11 +2,12 @@ const {db, fs} = require('../firebase-confing')
 const {addDoc, collection} = require('firebase/firestore')
 const postUser = async (req,res)=>{
     try {
-        const {uid, name, lastname, age, sex, country, photo, description, isOn, isVip, user, friends} = req.body;
+        const {uid,mail, name, lastname, age, sex, country, photo, description, isOn, isVip, user, friends} = req.body;
         const usersRef = collection(fs, "users")
         
        await addDoc(usersRef,{
         uid,
+        mail,
         name,
         lastname,
         age,
