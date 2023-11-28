a5a80722aaf04d5960e5e00fc05d03e85f358e36
Jose I. Martinez <joseignacio2534@gmail.com>
2023-11-28 02:48:03 -0300

    agregue simbolo de online/offline en /connect

--- client/src/components/Home/Users/Users.jsx
+++ client/src/components/Home/Users/Users.jsx
@@ -3,16 +3,16 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getOnline } from "../../../redux/actions/actions";
 
-import defaultImg from '../../../assets/user-default-pfp.png'
+import defaultImg from "../../../assets/user-default-pfp.png";
 
 import Searchbar from "../Searchbar/Searchbar";
 
-import styles from './Users.module.css'
+import styles from "./Users.module.css";
 
 const Users = () => {
   const users = useSelector((state) => state.users.users);
   const dispatch = useDispatch();
-  console.log(users)
+  console.log(users);
 
   useEffect(() => {
     dispatch(getOnline());
@@ -20,25 +20,47 @@ const Users = () => {
 
   return (
     <div className={styles.connectContainer}>
-    <Searchbar/>
-    <div className={styles.usersContainer}>
+      <Searchbar />
+      <div className={styles.usersContainer}>
         <h3>Online Users</h3>
-        {users.length === 0 ? ( 
+        {users.length === 0 ? (
           <p>Users not found</p>
         ) : (
           users.map((user) => (
             <div className={styles.userContainer} key={user.id}>
               <div className={styles.userImgContainer}>
-                <img src={typeof user.photo === 'string' ? user.photo: defaultImg} alt="default image" />
+                <img
+                  src={typeof user.photo === "string" ? user.photo : defaultImg}
+                  alt="default image"
+                />
               </div>
               <div className={styles.textContainer}>
                 <div className={styles.nameAndVipContainer}>
-                <p>{user.name}</p>
-                <p>vip</p>
+                  <p>{user.name}</p>
+                  <p>vip</p>
                 </div>
                 <div className={styles.restPropsContainer}>
-                <p>{user.country}</p>
-                <p>{user.sex}</p>
+                  <p>{user.country}</p>
+                  <p>{user.sex}</p>
+                  <div>
+                    {user.isOn ? (
+                      <span
+                        role="img"
+                        aria-label="Online"
+                        style={{ color: "green" }}
+                      >
+                        🟢
+                      </span>
+                    ) : (
+                      <span
+                        role="img"
+                        aria-label="Offline"
+                        style={{ color: "red" }}
+                      >
+                        🔴
+                      </span>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
