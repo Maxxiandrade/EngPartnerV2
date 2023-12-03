ee03dd185c294204f1bf7d0306f3a3696dc399f8
Luis Reyes <yuiftw05@gmail.com>
2023-12-03 16:56:48 -0600

    solucionando conflictos

--- client/src/redux/reducers/usersReducer.js
+++ client/src/redux/reducers/usersReducer.js
@@ -17,12 +17,9 @@ import {
   CLEAR_USER_DATA_IN_LOGOUT,
   GET_FRIENDS,
   CHANGE_USER,
-<<<<<<< HEAD
   SELECT_REPORT,
-=======
   CREATE_ROOM,
   GET_REPORTED
->>>>>>> f1defa3a0730660109374caa519cfe753c0c488e
 } from "../action_types/userActionTypes";
 
 const initialState = {
@@ -42,11 +39,8 @@ const initialState = {
   description: "",
   photo: "",
   friends: [],
-<<<<<<< HEAD
   reports: [],
-=======
   rooms:[],
->>>>>>> f1defa3a0730660109374caa519cfe753c0c488e
   isVip: false,
   isOn: false,
   isAdmin: false,
@@ -209,15 +203,15 @@ const usersReducer = (state = initialState, action) => {
             ? state.uid + action.payload.uid
             : action.payload.uid + state.uid,
       };
-<<<<<<< HEAD
 
       //report options
     case SELECT_REPORT:
       return{
         ...state,
         reports: action.payload
-=======
-    case CREATE_ROOM:
+      }
+    
+      case CREATE_ROOM:
       return{
         ...state,
         rooms:[... state.rooms, action.payload.nameGroup]
@@ -226,7 +220,6 @@ const usersReducer = (state = initialState, action) => {
       return{
         ...state,
         reported: action.payload
->>>>>>> f1defa3a0730660109374caa519cfe753c0c488e
       }
     default:
       return { ...state };
