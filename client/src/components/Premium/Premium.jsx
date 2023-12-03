9a0e3e6cb306690792586dd87290af85cbbfe214
Vicente <fordvicente1@gmail.com>
2023-12-03 01:30:07 -0300

    persistencia en premium

--- client/src/components/Premium/Premium.jsx
+++ client/src/components/Premium/Premium.jsx
@@ -1,19 +1,28 @@
 import { loadStripe } from "@stripe/stripe-js";
 import {Elements} from "@stripe/react-stripe-js";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 import style from "./premium.module.css";
 import AnnualPremium from "./AnnualPremium";
 import MonthPremium from "./MonthPremuim";
 import logo from "../../assets/logo-EngPartner.png";
+import { useEffect } from "react";
+import { getMyUser } from "../../redux/actions/actions";
+
 
 const stripePromise = loadStripe("pk_test_51OFi4pDa4OdRCPg7S1mBe55Usd8TeiSRRVlUiw6q3vJT7cHD7pdJqY5mdRaFBrmLMF9717TAW7Qg1GNXXfiTxzgF00K8IQSPkR");
 
 
 function Premiun() {
-  const uid = useSelector((state) => state.users.uid);
-  const isVip = useSelector((state) => state.users.isVip);  
+  const dispatch = useDispatch();
+  const uid = localStorage.getItem("uid");
+  const isVip = useSelector((state) => state.users.isVip); 
+  
+  useEffect(() => {
+    dispatch(getMyUser(uid));
+  }, [])
+  
     return (
       <div>
       <nav className={style.nav}>
