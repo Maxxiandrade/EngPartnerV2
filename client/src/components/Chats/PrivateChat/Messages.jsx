b167750658ec3e0d8831f419fd472bf2ed1094e9
Vicente <fordvicente1@gmail.com>
2023-12-07 17:31:27 -0300

    traduccion tiempo real en chats privados

--- client/src/components/Chats/PrivateChat/Messages.jsx
+++ client/src/components/Chats/PrivateChat/Messages.jsx
@@ -5,28 +5,41 @@ import { useSelector } from 'react-redux'
 import { onSnapshot, doc, collection, query } from 'firebase/firestore'
 import {db} from '../../../firebase-config'
 
-const Messages = () => {
+
+const Messages = ( {languageChecked, setLanguageChecked, handleChangeSwitch} ) => {
     const chatId = useSelector((state)=>state.users.chatId)
     const user = useSelector((state)=>state.users.userChat)
     const [messages, setMessages] = useState([])
 
     useEffect(() => {
-      if (chatId) { 
-        const unSub = onSnapshot(doc(db, "chats", chatId), (doc) => {
-          doc.exists() && setMessages(doc.data().messages);
-        });
+      if (chatId) {
+        const unSub = onSnapshot(
+          collection(db, 'messages'),
+          (querySnapshot) => {
+            const messages = [];
+            querySnapshot.forEach((doc) => {
+              // Verificar si el documento tiene la propiedad correcta
+              if (doc.exists() && doc.data().chatId === chatId) {
+                messages.push(doc.data());
+              }
+            });
+            messages.sort((a, b) => a.date.toDate() - b.date.toDate());
+
+            setMessages(messages);
+          }
+        );
     
         return () => {
           unSub();
         };
       }
-    }, [chatId]);
+    }, [chatId, setMessages]);
       console.log(messages)
 
   return (
     <div className={style.messages}>
       {messages?.map((m) => (
-        <Message message={m} key={m.id} />
+        <Message message={m} key={m.id} languageChecked={languageChecked} />
       ))}
       
     </div>
