55ee673c7cf685c22635784610196dcdfb84e8c5
Vicente <fordvicente1@gmail.com>
2023-12-12 00:15:19 -0300

    Fix switch

--- client/src/components/Chats/PrivateChat/Chat.jsx
+++ client/src/components/Chats/PrivateChat/Chat.jsx
@@ -23,13 +23,13 @@ const Chat = () => {
   }
 
   const MaterialUISwitch = styled(Switch)(({ theme }) => ({
-    width: 70,
-    height: 34,
+    width: 63,
+    height: 30,
     padding: 7,
     '& .MuiSwitch-switchBase': {
       margin: 1,
       padding: 0,
-      transform: 'translateX(6px)',
+      transform: 'translateX(2px)',
       '&.Mui-checked': {
         color: '#fff',
         transform: 'translateX(32px)',
@@ -43,8 +43,8 @@ const Chat = () => {
       backgroundColor:'#042c54',
       border: '2px solid #3c70a3',
       borderRadius: '50%',
-      width: 32,
-      height: 32,
+      width: 24,
+      height: 24,
       '&:before': {
         content: "''",
         position: 'absolute',
@@ -62,6 +62,7 @@ const Chat = () => {
       borderRadius: 20 / 2,
     },
   }));
+
 
   return (
     <div className={style.chat}>
