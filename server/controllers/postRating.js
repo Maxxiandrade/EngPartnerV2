301fa00568f92009054e897460570973d9ba244b
Maxxiandrade <maxiandrade62@gmail.com>
2023-12-14 03:44:28 -0300

    arreglando rating

--- server/controllers/postRating.js
+++ server/controllers/postRating.js
@@ -1,12 +1,15 @@
 const { db, fs } = require('../firebase-confing');
-const { updateDoc, doc, getDoc, setDoc, collection } = require('firebase/firestore');
+const { updateDoc, doc, getDoc, setDoc, collection, getDocs, query, where } = require('firebase/firestore');
 
 const postRating = async (req, res) => {
     try {
         const { rating, uid } = req.body;
         const usersCollectionRef = collection(fs, 'users');
-        // Resto del código para actualizar 'didRate'...
-
+        const querySnapshot = await getDocs(query(usersCollectionRef, where('uid', '==', uid)));
+        const userDoc = querySnapshot.docs[0];
+        await updateDoc(doc(fs, 'users', userDoc.id),{
+            didRate: true
+        });
         const ratingRef = doc(fs, 'rating', 'universalRatingDocument');
         const ratingDoc = await getDoc(ratingRef);
 
@@ -14,7 +17,7 @@ const postRating = async (req, res) => {
             await setDoc(ratingRef, { ratings: [rating] });
         } else {
             const { ratings } = ratingDoc.data();
-            const updatedRatings = [...ratings, rating]; // Agregar el nuevo rating al array
+            const updatedRatings = [...ratings, rating]; 
             await updateDoc(ratingRef, { ratings: updatedRatings });
         }
 
